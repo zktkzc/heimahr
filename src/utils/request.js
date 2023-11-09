55f22a918700fc16c76584bec1d0de9fca55569a
tkzc00 <2238983030@qq.com>
2023-11-09 15:38:57 +0800

    axios封装处理响应拦截器

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import store from "@/store";
+import {Message} from "element-ui";
 
 const service = axios.create({
   baseURL: '/api', // 基础地址,
   timeout: 10000 // 超时时间，单位是毫秒
 }) // 创建一个新的axios实例
 
+// 请求拦截器
 service.interceptors.request.use((config) => {
   // 注入token
   if (store.getters.token) {
@@ -14,6 +16,21 @@ service.interceptors.request.use((config) => {
   return config
 }, (error) => {
   // 失败执行promise
+  return Promise.reject(error)
+})
+
+// 响应拦截器
+service.interceptors.response.use((response) => {
+  // axios默认包裹了一层data
+  const {data, message, success} = response.data
+  if (success) {
+    return data
+  } else {
+    Message({type: 'error', message})
+    return Promise.reject(new Error(message))
+  }
+}, (error) => {
+  Message({type: 'error', message: error.message})
   return Promise.reject(error)
 })
 
