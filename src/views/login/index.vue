0132b540f707d7b2b3b4e5c903f6b74714ef66b2
tkzc00 <2238983030@qq.com>
2023-11-09 16:36:49 +0800

    登录联调跳转主页

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -31,9 +31,9 @@ export default {
   data() {
     return {
       loginForm: {
-        mobile: '',
-        password: '',
-        isAgree: false
+        mobile: process.env.NODE_ENV === 'development' ? '13800000002' : '',
+        password: process.env.NODE_ENV === 'development' ? 'hm#qd@23!' : '',
+        isAgree: process.env.NODE_ENV === 'development'
       },
       loginRules: {
         mobile: [{
@@ -71,9 +71,12 @@ export default {
   },
   methods: {
     login() {
-      this.$refs.form.validate((isOk) => {
+      this.$refs.form.validate(async (isOk) => {
         if (isOk) {
-          this.$store.dispatch('user/login', this.loginForm)
+          await this.$store.dispatch('user/login', this.loginForm)
+          // Vuex中的action返回的是Promise
+          // 跳转到主页
+          this.$router.push('/')
         }
       })
     }
