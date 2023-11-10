094837b6f6180671326b238b239c4e5858e5adbf
tkzc00 <2238983030@qq.com>
2023-11-10 17:07:54 +0800

    添加子部门的表单结构

--- src/views/department/components/add-dept.vue
+++ src/views/department/components/add-dept.vue
@@ -6,6 +6,9 @@ export default {
       default: false
     }
   },
+  data() {
+
+  },
   methods: {
     close() {
       // 修改父组件的值
@@ -16,8 +19,29 @@ export default {
 </script>
 
 <template>
-  <el-dialog title="新增部门" :visible="showDialog" @close="close">
-
+  <el-dialog title="添加子部门" :visible="showDialog" @close="close">
+    <el-form label-width="120px">
+      <el-form-item label="部门名称">
+        <el-input style="width: 80%;" placeholder="2-10个字符" size="mini"/>
+      </el-form-item>
+      <el-form-item label="部门编码">
+        <el-input style="width: 80%;" placeholder="2-10个字符" size="mini"/>
+      </el-form-item>
+      <el-form-item label="部门负责人">
+        <el-select style="width: 80%;" placeholder="请选择负责人" size="mini"/>
+      </el-form-item>
+      <el-form-item label="部门介绍">
+        <el-input style="width: 80%;" type="textarea" :rows="4" placeholder="1-100个字符" size="mini"/>
+      </el-form-item>
+      <el-form-item>
+        <el-row type="flex" justify="center">
+          <el-col :span="12">
+            <el-button type="primary" size="mini">确定</el-button>
+            <el-button size="mini">取消</el-button>
+          </el-col>
+        </el-row>
+      </el-form-item>
+    </el-form>
   </el-dialog>
 </template>
 
