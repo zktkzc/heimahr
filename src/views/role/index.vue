590e87b2e01c561388bb96d6e98ec072e97e0ba0
tkzc00 <2238983030@qq.com>
2023-11-11 12:58:19 +0800

    完成表格列的自定义结构

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -8,9 +8,20 @@
       <!-- 放置表格组件 -->
       <el-table :data="roleList">
         <el-table-column prop="name" width="200" label="角色" align="center"/>
-        <el-table-column prop="state" width="200" label="启用" align="center"/>
+        <el-table-column prop="state" width="200" label="启用" align="center">
+          <!-- 自定义列结构 -->
+          <template v-slot="{row}">
+            <span>{{ row.state === 1 ? '已启用' : row.state === 0 ? '未启用' : '无' }}</span>
+          </template>
+        </el-table-column>
         <el-table-column prop="description" label="描述" align="center"/>
-        <el-table-column label="操作" align="center"/>
+        <el-table-column label="操作" align="center">
+          <template>
+            <el-button type="text" size="mini">分配权限</el-button>
+            <el-button type="text" size="mini">编辑</el-button>
+            <el-button type="text" size="mini">删除</el-button>
+          </template>
+        </el-table-column>
       </el-table>
       <!--放置分页组件-->
       <el-row type="flex" justify="end" align="middle" style="height: 60px;">
