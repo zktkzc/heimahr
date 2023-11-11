c9e670d37f1a0812a95ea6ccdaa8203585863f27
tkzc00 <2238983030@qq.com>
2023-11-11 11:56:12 +0800

    获取角色数据

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -6,10 +6,10 @@
         <el-button type="primary" size="mini">添加角色</el-button>
       </div>
       <!-- 放置表格组件 -->
-      <el-table>
-        <el-table-column width="200" label="角色" align="center"/>
-        <el-table-column width="200" label="启用" align="center"/>
-        <el-table-column label="描述" align="center"/>
+      <el-table :data="roleList">
+        <el-table-column prop="name" width="200" label="角色" align="center"/>
+        <el-table-column prop="state" width="200" label="启用" align="center"/>
+        <el-table-column prop="description" label="描述" align="center"/>
         <el-table-column label="操作" align="center"/>
       </el-table>
       <!--放置分页组件-->
@@ -20,8 +20,24 @@
   </div>
 </template>
 <script>
+import {getRoleList} from "@/api/role"
+
 export default {
-  name: 'Role'
+  name: 'Role',
+  created() {
+    this.getRoleList()
+  },
+  data() {
+    return {
+      roleList: []
+    }
+  },
+  methods: {
+    async getRoleList() {
+      const {rows} = await getRoleList()
+      this.roleList = rows
+    }
+  }
 }
 </script>
 <style scoped>
