f519e71febc866b1600630082343d34108f17d4d
tkzc00 <2238983030@qq.com>
2023-11-11 19:51:57 +0800

    员工表格和分页结构

--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -16,7 +16,26 @@
           <el-button size="mini">excel导出</el-button>
         </el-row>
         <!-- 表格组件 -->
+        <el-table>
+          <el-table-column align="center" label="头像"></el-table-column>
+          <el-table-column label="姓名"></el-table-column>
+          <el-table-column label="手机号" sortable></el-table-column>
+          <el-table-column label="工号" sortable></el-table-column>
+          <el-table-column label="聘用形式"></el-table-column>
+          <el-table-column label="部门"></el-table-column>
+          <el-table-column label="入职时间" sortable></el-table-column>
+          <el-table-column label="操作">
+            <template>
+              <el-button size="mini" type="text">查看</el-button>
+              <el-button size="mini" type="text">角色</el-button>
+              <el-button size="mini" type="text">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
         <!-- 分页 -->
+        <el-row align="middle" justify="end" style="height: 60px;" type="flex">
+          <el-pagination :total="1000" layout="total, prev, pager, next"/>
+        </el-row>
       </div>
     </div>
   </div>
