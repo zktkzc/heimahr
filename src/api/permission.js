29d7be9f7a5d84a0be80897ec40812517cd27275
tkzc00 <2238983030@qq.com>
2023-11-12 18:50:26 +0800

    获取权限数据

---
+++ src/api/permission.js
@@ -0,0 +1,10 @@
+import request from "@/utils/request"
+
+/**
+ * 获取权限列表
+ */
+export function getPermissionList() {
+  return request({
+    url: '/sys/permission'
+  })
+}
--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -2,22 +2,40 @@
   <div class="container">
     <div class="app-container">
       <el-button class="btn-add" size="mini" type="primary">添加权限</el-button>
-      <el-table>
-        <el-table-column label="名称"/>
-        <el-table-column label="标识"/>
-        <el-table-column label="描述"/>
+      <el-table :data="list" default-expand-all row-key="id">
+        <el-table-column label="名称" prop="name"/>
+        <el-table-column label="标识" prop="code"/>
+        <el-table-column label="描述" prop="description"/>
         <el-table-column label="操作">
-          <el-button size="mini" type="text">添加</el-button>
-          <el-button size="mini" type="text">编辑</el-button>
-          <el-button size="mini" type="text">删除</el-button>
+          <template v-slot="{row}">
+            <el-button v-if="row.type === 1" size="mini" type="text">添加</el-button>
+            <el-button size="mini" type="text">编辑</el-button>
+            <el-button size="mini" type="text">删除</el-button>
+          </template>
         </el-table-column>
       </el-table>
     </div>
   </div>
 </template>
 <script>
+import {getPermissionList} from "@/api/permission"
+import {transListToTreeData} from "@/utils"
+
 export default {
-  name: 'Permission'
+  name: 'Permission',
+  data() {
+    return {
+      list: []
+    }
+  },
+  created() {
+    this.getPermissionList()
+  },
+  methods: {
+    async getPermissionList() {
+      this.list = transListToTreeData(await getPermissionList(), 0)
+    }
+  }
 }
 </script>
 <style scoped>
