c55529ce9d12503d75155cee9ccf8d104e6353f8
tkzc00 <2238983030@qq.com>
2023-11-12 10:32:49 +0800

    excel导入组件的封装

---
+++ src/views/employee/components/import-excel.vue
@@ -0,0 +1,83 @@
+<template>
+  <el-dialog
+    :visible="showExcelDialog"
+    title="员工导入"
+    width="500px"
+    @close="$emit('update:showExcelDialog', false)"
+  >
+    <el-row justify="center" type="flex">
+      <div class="upload-excel">
+        <input
+          ref="excel-upload-input"
+          accept=".xlsx, .xls"
+          class="excel-upload-input"
+          type="file"
+        >
+        <div class="drop">
+          <i class="el-icon-upload"/>
+          <el-button type="text">下载导入模板</el-button>
+          <span>将文件拖到此处或
+            <el-button type="text">点击上传</el-button>
+          </span>
+        </div>
+      </div>
+    </el-row>
+    <el-row justify="end" type="flex">
+      <!-- update:props属性名，值 直接修改 .sync修饰符的属性值 -->
+      <el-button size="mini" type="primary" @click="$emit('update:showExcelDialog', false)">取消</el-button>
+    </el-row>
+  </el-dialog>
+</template>
+<script>
+
+export default {
+  props: {
+    showExcelDialog: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.upload-excel {
+  display: flex;
+  justify-content: center;
+  margin: 20px;
+  width: 360px;
+  height: 180px;
+  align-items: center;
+  color: #697086;
+
+  .excel-upload-input {
+    display: none;
+    z-index: -9999;
+  }
+
+  .btn-upload,
+  .drop {
+    border: 1px dashed #dcdfe6;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    line-height: 160px;
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .drop {
+    line-height: 40px;
+    color: #bbb;
+
+    i {
+      font-size: 60px;
+      display: block;
+      color: #c0c4cc;
+    }
+  }
+}
+</style>
--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -11,7 +11,7 @@
       <div class="right">
         <el-row class="opeate-tools" justify="end" type="flex">
           <el-button size="mini" type="primary">添加员工</el-button>
-          <el-button size="mini">excel导入</el-button>
+          <el-button size="mini" @click="showExcelDialog=true">excel导入</el-button>
           <el-button size="mini" @click="exportEmployee">excel导出</el-button>
         </el-row>
         <!-- 表格组件 -->
@@ -49,6 +49,8 @@
         </el-row>
       </div>
     </div>
+    <!-- 放置excel导入组件 -->
+    <ImportExcel :show-excel-dialog.sync="showExcelDialog"/>
   </div>
 </template>
 
@@ -57,9 +59,13 @@ import {getDepartment} from "@/api/department"
 import {transListToTreeData} from "@/utils"
 import {exportEmployee, getEmployeeList} from "@/api/employee"
 import FileSaver from 'file-saver'
+import ImportExcel from "@/views/employee/components/import-excel.vue"
 
 export default {
   name: 'Employee',
+  components: {
+    ImportExcel
+  },
   data() {
     return {
       depts: [], // 组织数据
@@ -75,6 +81,7 @@ export default {
       },
       list: [], // 存储员工列表数据
       total: 0, // 记录员工的总数
+      showExcelDialog: false //控制Excel导入弹层的显示
     }
   },
   created() {
