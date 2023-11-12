a0e574dc62ba102fb9c23b2aab0354039bfd3446
tkzc00 <2238983030@qq.com>
2023-11-12 15:35:22 +0800

    完成级联组件的双向绑定

--- src/views/employee/components/select-tree.vue
+++ src/views/employee/components/select-tree.vue
@@ -4,6 +4,12 @@ import {getDepartment} from "@/api/department"
 import {transListToTreeData} from "@/utils"
 
 export default {
+  props: {
+    value: {
+      type: Number, // 存储部门id
+      default: 0
+    }
+  },
   data() {
     return {
       treeData: [],
@@ -19,13 +25,21 @@ export default {
   methods: {
     async getDepartment() {
       this.treeData = transListToTreeData(await getDepartment(), 0)
+    },
+    changeValue(list) {
+      // 取到数组的最后一位
+      if (list.length > 0) {
+        this.$emit('input', list[list.length - 1]) // 将最后一位的id取出来，传出去
+      } else {
+        this.$emit('input', null) // 如果长度为0，说明值为空
+      }
     }
   }
 }
 </script>
 
 <template>
-  <el-cascader :options="treeData" :props="props" separator="-" size="mini"/>
+  <el-cascader :options="treeData" :props="props" :value="value" separator="-" size="mini" @change="changeValue"/>
 </template>
 
 <style lang="scss" scoped>
